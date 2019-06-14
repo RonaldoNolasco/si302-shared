73678eda49bd3d6527657dc7213b0937a0257112
Ronaldo Nolasco <rnolascoc@uni.pe>
2019-06-14 14:50:13 -0500

    Actualizando

---
+++ payFast/src/main/java/pe/edu/uni/fiis/payFast/service/usuario/UsuarioService.java
@@ -0,0 +1,7 @@
+package pe.edu.uni.fiis.payFast.service.usuario;
+
+import pe.edu.uni.fiis.payFast.model.Usuario;
+
+public interface UsuarioService {
+    public Usuario agregarUsuario(Usuario usuario);
+}
