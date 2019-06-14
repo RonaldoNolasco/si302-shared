f495d39020956d271b777f927101f8515b3a5a54
Ronaldo Nolasco <rnolascoc@uni.pe>
2019-06-14 02:46:31 -0500

    Carpeta

---
+++ edukid/src/main/java/pe/edu/uni/fiis/edukid/controller/UsuarioController.java
@@ -0,0 +1,32 @@
+package pe.edu.uni.fiis.edukid.controller;
+
+import pe.edu.uni.fiis.edukid.dao.SingletonUsuarioDao;
+import pe.edu.uni.fiis.edukid.model.Usuario;
+import pe.edu.uni.fiis.edukid.service.SingletonService;
+import pe.edu.uni.fiis.edukid.service.usuario.UsuarioService;
+import pe.edu.uni.fiis.edukid.service.usuario.impl.UsuarioServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UsuarioController",urlPatterns = {"/registro-usuario"})
+public class UsuarioController extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      String codigo=req.getParameter("cod");
+      String nombre=req.getParameter("nom");
+      String credencial=req.getParameter("cred");
+      String correo=req.getParameter("cor");
+        Usuario usur1= new Usuario(codigo,nombre,credencial,correo);
+
+        SingletonService.getUsuarioService().agregarUsuario(usur1);
+        resp.getWriter().write("Se guardo el usuario correctamente");
+    }
+
+    public static void main(String[] args) {
+    }
+}
