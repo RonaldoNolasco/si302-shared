6ea703e3ca5ef9a87f133f212a8a0d34413469d2
Ronaldo Nolasco <rnolascoc@uni.pe>
2019-06-14 19:50:53 -0500

    borrando

--- edukid/src/main/java/pe/edu/uni/fiis/edukid/dao/usuario/UsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.uni.fiis.edukid.dao.usuario;
-
-import pe.edu.uni.fiis.edukid.model.Usuario;
-
-import java.sql.Connection;
-
-public interface UsuarioDao {
-    public Usuario agregarUsuario(Usuario a, Connection b);
-}
