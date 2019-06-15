8bc1bf8fe87bfdb7fe53b9b523e0f445f0ae3969
Ronaldo Nolasco <rnolascoc@uni.pe>
2019-06-14 19:51:46 -0500

    s12

---
+++ S12/edukid/src/main/java/pe/edu/uni/fiis/edukid/service/SingletonService.java
@@ -0,0 +1,15 @@
+package pe.edu.uni.fiis.edukid.service;
+
+import pe.edu.uni.fiis.edukid.service.usuario.UsuarioService;
+import pe.edu.uni.fiis.edukid.service.usuario.impl.UsuarioServiceImpl;
+
+public class SingletonService {
+    private static UsuarioService usuarioService = null;
+
+    public static UsuarioService getUsuarioService() {
+        if (usuarioService == null) {
+            usuarioService = new UsuarioServiceImpl();
+        }
+        return usuarioService;
+    }
+}
