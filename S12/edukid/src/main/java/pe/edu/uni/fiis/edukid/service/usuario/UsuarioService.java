f255ad36e9725e49e1a5f61d4ce98f8c9b9c64da
Ronaldo Nolasco <rnolascoc@uni.pe>
2019-06-16 19:05:59 -0500

    Update

--- S12/edukid/src/main/java/pe/edu/uni/fiis/edukid/service/usuario/UsuarioService.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.uni.fiis.edukid.service.usuario;
-
-import pe.edu.uni.fiis.edukid.dao.SingletonUsuarioDao;
-import pe.edu.uni.fiis.edukid.model.Usuario;
-
-public interface UsuarioService {
-    public Usuario agregarUsuario(Usuario usuario);
-}
